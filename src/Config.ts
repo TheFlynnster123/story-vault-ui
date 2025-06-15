51624cd4b2cafb3d1f98a7c259b7dfecdb513879
TheFlynnster <reereeree123@atomicmail.io>
2025-06-15 14:24:41 -0500

    Add console log

--- src/Config.ts
+++ src/Config.ts
@@ -2,5 +2,5 @@ const config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
 };
-
+console.log("ENV VARS:", import.meta.env);
 export default config;
